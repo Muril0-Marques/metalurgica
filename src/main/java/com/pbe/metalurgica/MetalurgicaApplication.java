438a89036fab7e7ebe9849029ac3426f77ba0b2f
Muril0-Marques <marques.murilo397@gmail.com>
2026-02-09 14:21:42 -0300

    Atividade Spring Boot em JAVA

---
+++ src/main/java/com/pbe/metalurgica/MetalurgicaApplication.java
@@ -0,0 +1,13 @@
+package com.pbe.metalurgica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalurgicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetalurgicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbe/metalurgica/MetalurgicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbe.metalurgica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalurgicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
